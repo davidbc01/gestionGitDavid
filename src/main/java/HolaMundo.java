afd44425df8fa6228278b7b80a5b6a9b893a32df
DESKTOP-47ANFPA\daw2 <dbartolc01@iescastelar.com>
2023-09-13 20:05:46 +0200

    Bug solution

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -4,5 +4,6 @@ public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("Esto es código nuevo");
+        System.out.println("Esto era un bug");
     }
 }
