0e4ad73982a78d771acac9d938b0aff7aa0965ad
DESKTOP-47ANFPA\daw2 <dbartolc01@iescastelar.com>
2023-09-13 19:35:41 +0200

    New change

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -3,5 +3,6 @@ package src.main.java;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Esto es código nuevo");
     }
 }
